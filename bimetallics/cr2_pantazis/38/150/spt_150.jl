03bd339c7eb18f00f1079e410bd8016d02745ab5
arnab82 <arnab22@vt.edu>
2024-09-10 13:20:20 -0400

    M=150,200 variational results added for cr2 dimer

---
+++ bimetallics/cr2_pantazis/38/150/spt_150.jl
@@ -0,0 +1,70 @@
+using QCBase
+using FermiCG
+using NPZ
+using InCoreIntegrals
+using RDM
+using JLD2
+using Printf
+
+@load "data_cmf.jld2"
+
+M = 150
+display(clusters)
+display(init_fspace)
+init_fspace = FermiCG.FockConfig(init_fspace)
+cluster_bases = FermiCG.compute_cluster_eigenbasis_spin(ints, clusters, d1, [3,3,3,3,3], init_fspace, max_roots=M, verbose=1);
+
+clustered_ham = FermiCG.extract_ClusteredTerms(ints, clusters);
+cluster_ops = FermiCG.compute_cluster_ops(cluster_bases, ints);
+
+FermiCG.add_cmf_operators!(cluster_ops, cluster_bases, ints, d1.a, d1.b);
+
+nroots=4
+# start by defining P/Q spaces
+p_spaces = Vector{ClusterSubspace}()
+ssi = ClusterSubspace(clusters[1])
+add_subspace!(ssi, (2,5), 1:1)
+add_subspace!(ssi, (3,4), 1:1)
+add_subspace!(ssi, (4,3), 1:1)
+add_subspace!(ssi, (5,2), 1:1)
+push!(p_spaces, ssi)
+
+
+ssi = ClusterSubspace(clusters[2])
+add_subspace!(ssi, (2,5), 1:1)
+add_subspace!(ssi, (3,4), 1:1)
+add_subspace!(ssi, (4,3), 1:1)
+add_subspace!(ssi, (5,2), 1:1)
+push!(p_spaces, ssi)
+ssi = ClusterSubspace(clusters[3])
+add_subspace!(ssi, init_fspace[3], 1:1)
+push!(p_spaces, ssi)
+ssi = ClusterSubspace(clusters[4])
+add_subspace!(ssi, init_fspace[4], 1:1)
+push!(p_spaces, ssi)
+ssi = ClusterSubspace(clusters[4])
+add_subspace!(ssi, init_fspace[4], 1:1)
+push!(p_spaces, ssi)
+    
+
+
+ci_vector = BSTstate(clusters, p_spaces, cluster_bases, R=4) 
+
+na = sum([i[1] for i in init_fspace]) 
+nb = sum([i[2] for i in init_fspace]) 
+
+FermiCG.fill_p_space!(ci_vector, na, nb)
+FermiCG.eye!(ci_vector)
+e_ci, v = FermiCG.ci_solve(ci_vector, cluster_ops, clustered_ham);
+
+
+bst_energy, v = FermiCG.block_sparse_tucker(v, cluster_ops, clustered_ham,max_iter=200, thresh_var=8e-3, thresh_foi=5e-5,thresh_pt=1e-3,thresh_spin=9e-4);
+@save "/home/arnab22/SPF-data/cr2_pantazis/rohf.def2svp/38__3d4d_2p3p/spin_corrected/data_bst_150.jld2" clusters init_fspace ints cluster_bases v
+bst_energy, v = FermiCG.block_sparse_tucker(v, cluster_ops, clustered_ham,max_iter=200, thresh_var=8e-3, thresh_foi=2e-5,thresh_pt=1e-3,thresh_spin=9e-4);
+@save "/home/arnab22/SPF-data/cr2_pantazis/rohf.def2svp/38__3d4d_2p3p/spin_corrected/data_bst_150.jld2" clusters init_fspace ints cluster_bases v
+
+FermiCG.compute_pt2_energy(v, cluster_ops, clustered_ham, thresh_foi=1e-4,prescreen=true,compress_twice=true,verbose=2 )
+
+
+
+
---
+++ bimetallics/cr2_pantazis/38/200/spt_200_3.jl
@@ -0,0 +1,38 @@
+using QCBase
+using FermiCG
+using NPZ
+using InCoreIntegrals
+using RDM
+using JLD2
+using Printf
+
+@load "data_cmf.jld2"
+
+M = 200
+display(clusters)
+display(init_fspace)
+init_fspace = FermiCG.FockConfig(init_fspace)
+#cluster_bases = FermiCG.compute_cluster_eigenbasis_spin(ints, clusters, d1, [3,3,3,3,3], init_fspace, max_roots=M, verbose=1);
+@load "data_bst_200.jld2"
+clustered_ham = FermiCG.extract_ClusteredTerms(ints, clusters);
+cluster_ops = FermiCG.compute_cluster_ops(cluster_bases, ints);
+
+FermiCG.add_cmf_operators!(cluster_ops, cluster_bases, ints, d1.a, d1.b);
+
+nroots=4
+
+
+bst_energy, v = FermiCG.block_sparse_tucker(v, cluster_ops, clustered_ham,max_iter=5, thresh_var=7e-3, thresh_foi=3e-5,thresh_pt=9.5e-4,thresh_spin=9.5e-4);
+@save "/home/arnab22/SPF-data/cr2_pantazis/rohf.def2svp/38__3d4d_2p3p/spin_corrected/data_bst_200.jld2" clusters init_fspace ints cluster_bases v
+bst_energy, v = FermiCG.block_sparse_tucker(v, cluster_ops, clustered_ham,max_iter=5, thresh_var=7e-3, thresh_foi=3e-5,thresh_pt=9.5e-4,thresh_spin=9.5e-4);
+@save "/home/arnab22/SPF-data/cr2_pantazis/rohf.def2svp/38__3d4d_2p3p/spin_corrected/data_bst_200.jld2" clusters init_fspace ints cluster_bases v
+bst_energy, v = FermiCG.block_sparse_tucker(v, cluster_ops, clustered_ham,max_iter=5, thresh_var=7e-3, thresh_foi=3e-5,thresh_pt=9.5e-4,thresh_spin=9.5e-4);
+@save "/home/arnab22/SPF-data/cr2_pantazis/rohf.def2svp/38__3d4d_2p3p/spin_corrected/data_bst_200.jld2" clusters init_fspace ints cluster_bases v
+bst_energy, v = FermiCG.block_sparse_tucker(v, cluster_ops, clustered_ham,max_iter=5, thresh_var=7e-3, thresh_foi=3e-5,thresh_pt=9.5e-4,thresh_spin=9.5e-4);
+@save "/home/arnab22/SPF-data/cr2_pantazis/rohf.def2svp/38__3d4d_2p3p/spin_corrected/data_bst_200.jld2" clusters init_fspace ints cluster_bases v
+bst_energy, v = FermiCG.block_sparse_tucker(v, cluster_ops, clustered_ham,max_iter=5, thresh_var=7e-3, thresh_foi=3e-5,thresh_pt=9.5e-4,thresh_spin=9.5e-4);
+@save "/home/arnab22/SPF-data/cr2_pantazis/rohf.def2svp/38__3d4d_2p3p/spin_corrected/data_bst_200.jld2" clusters init_fspace ints cluster_bases v
+bst_energy, v = FermiCG.block_sparse_tucker(v, cluster_ops, clustered_ham,max_iter=0, thresh_var=7e-3, thresh_foi=3e-5,thresh_pt=9.5e-4,thresh_spin=9.5e-4);
+@save "/home/arnab22/SPF-data/cr2_pantazis/rohf.def2svp/38__3d4d_2p3p/spin_corrected/data_bst_200.jld2" clusters init_fspace ints cluster_bases v
+bst_energy, v = FermiCG.block_sparse_tucker(v, cluster_ops, clustered_ham,max_iter=5, thresh_var=7e-3, thresh_foi=3e-5,thresh_pt=9.5e-4,thresh_spin=9.5e-4);
+@save "/home/arnab22/SPF-data/cr2_pantazis/rohf.def2svp/38__3d4d_2p3p/spin_corrected/data_bst_200.jld2" clusters init_fspace ints cluster_bases v
